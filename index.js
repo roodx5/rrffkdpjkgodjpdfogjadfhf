be9c9c1c28612b35193dd285ffe6aac292dd0590
roodx <42224407+roodx5@users.noreply.github.com>
2020-09-18 17:31:46 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: POULAZ (Angry Black)
+
+const Discord = require("discord.js");
+const cmd = require("node-cmd")
+const client = new Discord.Client();
+
+
+
+
+client.on('message', message=>
+{
+  var channelid = message.guild.channels.find('name','orders')
+  var prefix = '!'
+  var args = message.content.split(' ').slice('1').join(' ');
+  if(message.content.startsWith(prefix + 'req'))
+  {
+    var embed = new Discord.RichEmbed()
+    .setTitle(`طلب جديد من : ${message.author.username}`)
+    .setDescription(`${args}`)
+    channelid.sendEmbed(embed).then(m=>
+      {
+        message.reply('** <a:3307_verif_red:756279703266132078> تم ارسال طلبك انتظر حتا نرد عليك**')
+        
+      })
+
+  }
+})
+client.login(process.env.BOT_TOKEN);
