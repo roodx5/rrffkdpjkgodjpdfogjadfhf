a2de52c5927a67bcf17ad2666a87535525f65ab4
roodx <42224407+roodx5@users.noreply.github.com>
2020-11-06 19:02:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ client.on('message' , roodx=>{
 
     if(roodx.author.bot)return;
     if(roodx.channel.id !== '774249845925347328') return;
-    if(roodx.channel.id !== '774250173848223744') return;
-    if(roodx.channel.id !== '774250093246808095') return;
     if(roodx)
     {
         roodx.channel.send('https://media.discordapp.net/attachments/754398470558842930/759303476885979146/42_E25EB2C.gif')
     }
-})
+});
 
 
 client.on('message' , roodx=>{  
@@ -23,7 +21,7 @@ client.on('message' , roodx=>{
     {
         roodx.channel.send('https://media.discordapp.net/attachments/754398470558842930/759303476885979146/42_E25EB2C.gif')
     }
-})
+});
 
 
 client.on('message' , roodx=>{  
@@ -35,6 +33,6 @@ client.on('message' , roodx=>{
     {
         roodx.channel.send('https://media.discordapp.net/attachments/754398470558842930/759303476885979146/42_E25EB2C.gif')
     }
-})
+});
 
 client.login(process.env.BOT_TOKEN);
