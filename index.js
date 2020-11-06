0a7a2cb09b9954858e3905d7ae6a4d2a1719d972
roodx <42224407+roodx5@users.noreply.github.com>
2020-11-06 18:59:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ client.on('message' , roodx=>{
 })
 
 
+client.on('message' , roodx=>{  
+
+    if(roodx.author.bot)return;
+    if(roodx.channel.id !== '774250173848223744') return;
+    if(roodx)
+    {
+        roodx.channel.send('https://media.discordapp.net/attachments/754398470558842930/759303476885979146/42_E25EB2C.gif')
+    }
+})
 
 
+client.on('message' , roodx=>{  
+
+    if(roodx.author.bot)return;
+   
+    if(roodx.channel.id !== '774250093246808095') return;
+    if(roodx)
+    {
+        roodx.channel.send('https://media.discordapp.net/attachments/754398470558842930/759303476885979146/42_E25EB2C.gif')
+    }
+})
 
 client.login(process.env.BOT_TOKEN);
