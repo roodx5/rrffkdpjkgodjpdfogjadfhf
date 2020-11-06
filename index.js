bd41cae3fdbe3ab42caa389f6f06d855eda75817
roodx <42224407+roodx5@users.noreply.github.com>
2020-11-06 18:52:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ client.on('message' , roodx=>{
 
     if(roodx.author.bot)return;
     if(roodx.channel.id !== '774249845925347328') return;
+    if(roodx.channel.id !== '774250173848223744') return;
+    if(roodx.channel.id !== '774250093246808095') return;
     if(roodx)
     {
         roodx.channel.send('https://media.discordapp.net/attachments/754398470558842930/759303476885979146/42_E25EB2C.gif')
